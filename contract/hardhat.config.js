8e0a17bf26e3528173438c76aeae3b3383bb1ccd
Sasi <me@sasi.codes>
2022-03-05 11:56:28 +0530

    monorepo for project

--- contract/hardhat.config.js
+++ contract/hardhat.config.js
@@ -1,15 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
-require('dotenv').config()
 require("@nomiclabs/hardhat-etherscan");
-
-
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+require('dotenv').config()
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -18,7 +9,7 @@ module.exports = {
   solidity: "0.8.12",
   networks: {
     mumbai: {
-      url: process.env.NEXT_PUBLIC_RPC_URL,
+      url: process.env.RPC_URL,
       accounts: [process.env.PRIVATE_KEY]
     }
   },
